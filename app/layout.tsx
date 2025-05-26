f58f887a40cfd8b7e017c855b4187aa49ef3a6ef
flfang9 <flfang9@gmail.com>
2025-05-26 14:13:24 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "Let's Hang",
   description: "Plan hangouts with friends",
+    generator: 'v0.dev'
 }
 
 export default function RootLayout({
